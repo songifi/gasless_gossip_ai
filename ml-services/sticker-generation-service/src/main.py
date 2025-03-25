785e4f92da0dd8d64ad869c4381cb0d4676de71e
gabriel <gabrielwill666@gmail.com>
2025-03-25 01:41:15 +0100

    main script for sticker generation

--- ml-services/sticker-generation-service/src/main.py
+++ ml-services/sticker-generation-service/src/main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from sticker-generation-service!")
+"""Main Module for the Sticker Generation Service"""
 
+from diffusion_model import generate_image_from_prompt
+from utils import create_sticker, show_sticker_from_image
+
+def main():
+    '''main function'''
+    prompt = input("Enter prompt:")
+    image = generate_image_from_prompt(prompt)
+    sticker = create_sticker(image)
+    show_sticker_from_image(sticker, prompt)
 
 if __name__ == "__main__":
     main()
